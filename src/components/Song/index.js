43ea65bbd2e4a881edf99cd41c4ed6f7a13f9392
Muhhabbir <muhhabbir@gmail.com>
2022-04-01 15:52:06 +0700

    pr gg 8

--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,18 +1,23 @@
-const Song = ({ image, title, album }) => {
-    return (
-      <div className="playlist-container">
-        <div className="playlist-item">
-          <img className="song-image" src={image} alt="Album" />
-          <div className="song-desc">
-            <h5 className="song-title">{title}</h5>
-            <p className="song-album">{album}</p>
-          </div>
+const Song = ({ uri, image, title, album, selectState, isSelected }) => {
+  return (
+    <div className="playlist-container">
+      <div className="playlist-item">
+        <img className="song-image" src={image} alt="Album" />
+        <div className="song-desc">
+          <h5 className="song-title">{title}</h5>
+          <p className="song-album">{album}</p>
         </div>
-        <button className="playlist-action">
-          Select
-        </button>
       </div>
-    );
-  };
-  
-  export default Song;
+      <button
+        className="playlist-action"
+        onClick={() => {
+          selectState(uri);
+        }}
+      >
+        {isSelected ? "Deselect" : "Select"}
+      </button>
+    </div>
+  );
+};
+
+export default Song;
